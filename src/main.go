9a67334e4605a08cc534825f60ea7da293d687ae
MrKren <krengaming@gmail.com>
2022-06-25 15:51:14 +0100

    lint

--- src/main.go
+++ src/main.go
@@ -8,12 +8,12 @@ import (
 )
 
 func main() {
-    router := gin.Default()
-    router.Use(static.Serve("/", static.LocalFile("../frontend/build", true)))
-    router.StaticFile("/logo", "../frontend/src/logo.svg")
+	router := gin.Default()
+	router.Use(static.Serve("/", static.LocalFile("../frontend/build", true)))
+	router.StaticFile("/logo", "../frontend/src/logo.svg")
 
-    err := router.Run("0.0.0.0:8080")
-    if err != nil {
-        fmt.Print(err)
-    }
+	err := router.Run("0.0.0.0:8080")
+	if err != nil {
+		fmt.Print(err)
+	}
 }
