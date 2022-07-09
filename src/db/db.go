1b828aaae77d7f4e860ec611be9ec103902107fc
MrKren <krengaming@gmail.com>
2022-07-09 01:09:41 +0100

    Rename DB and add healthcheck

--- src/db/db.go
+++ src/db/db.go
@@ -11,7 +11,7 @@ import (
 func Init() *gorm.DB {
 	// TODO: add environment variable manager for production etc.
 	// db:3306 is the db address on the docker-compose network
-	db_url := "root:root@tcp(db:3306)/test_db?charset=utf8mb4&parseTime=True&loc=Local"
+	db_url := "root:root@tcp(db:3306)/quantchef"
 
 	db, err := gorm.Open(mysql.Open(db_url), &gorm.Config{})
 
